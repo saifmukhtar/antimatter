a320edc10396ebd9ed96a9b57b9e498ae9ac40e7
Saif Mukhtar <158864675+saifmukhtar@users.noreply.github.com>
2026-07-01 11:24:08 +0530

    Delete test_uri.kt

--- test_uri.kt
+++
@@ -1,7 +0,0 @@
-import java.net.URI
-fun main() {
-    val q = "https://example.com/?pubKey=hello%2Bworld+stuff"
-    val uri = URI.create(q)
-    val query = uri.query
-    println("Query: " + query)
-}
